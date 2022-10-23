c8b760216e05fc6f540354e260a79da03f962a15
Jonas Souza <jonasmzsouza@gmail.com>
2022-10-22 23:52:47 -0300

    update(frontweb): retrieving stores from backend

--- frontweb/src/components/filter/index.tsx
+++ frontweb/src/components/filter/index.tsx
@@ -1,17 +1,30 @@
+import { useEffect, useState } from 'react';
 import Select from 'react-select';
+import { requestBackend } from '../../utils/requests';
+import { Store } from '../../types/store';
 import './styles.css';
 
-const stores = [
-  { value: 'loja1', label: 'Loja 1' },
-  { value: 'loja2', label: 'Loja 2' },
-  { value: 'loja3', label: 'Loja 3' }
-];
-
 const Filter = () => {
+  const [selectStores, setSelectStores] = useState<Store[]>([]);
+
+  useEffect(() => {
+    requestBackend({ url: '/stores' }).then((response) => {
+      console.log(response.data);
+      setSelectStores(response.data);
+    });
+  }, []);
+
   return (
     <div className="filter-container base-card">
       <form className="filter-form">
-        <Select options={stores} isClearable placeholder="Loja" classNamePrefix="filter-select" />
+        <Select
+          options={selectStores}
+          isClearable
+          placeholder="Loja"
+          classNamePrefix="filter-select"
+          getOptionLabel={(store: Store) => store.name}
+          getOptionValue={(store: Store) => String(store.id)}
+        />
       </form>
     </div>
   );
